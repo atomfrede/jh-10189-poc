ad194353506a8ac4326257115f790099ebc8633c
Frederik Hahne <atomfrede@gmail.com>
2019-08-28 22:38:57 +0200

    :sparkles: very simple shell fragment service

---
+++ shell/src/main/java/shell/FragmentView.java
@@ -0,0 +1,17 @@
+package shell;
+
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.views.View;
+
+@Controller
+public class FragmentView {
+
+    @View("fragment")
+    @Get
+    public HttpResponse fragment() {
+
+        return HttpResponse.ok();
+    }
+}
